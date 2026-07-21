dba904d64920130da5b35dab8d15dfa1808f77bf
n1ck-L <nikitalut@yandex.ru>
2026-07-21 16:44:14 +0300

    добавлена общая ошибка NotFound для сервисов

--- app/services/exceptions.py
+++ app/services/exceptions.py
@@ -1,4 +1,4 @@
 
-class UserNotFound(Exception):
-    """Задача не найдна в БД"""
-    
+class NotFoundError(Exception):
+    def __init__(self, entity: str, entity_id: str):
+        super().__init__(f"{entity} с id={entity_id} не найден(-a)")
