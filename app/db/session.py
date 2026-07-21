92a37114603a5338cfc29e207af4ef9541d39f3a
n1ck-L <nikitalut@yandex.ru>
2026-07-21 15:45:13 +0300

    подключение postgresql

---
+++ app/db/session.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+from app.schemas.settings import SettingsSchema
+
+settings = SettingsSchema()
+engine = create_engine(str(settings.DATABASE_URL))
+SessionLocal = sessionmaker[Session](bind=engine)
+
+def get_db():
+    '''Функция для инъекции сессии базы данных'''
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ app/schemas/settings.py
@@ -0,0 +1,13 @@
+from pydantic import PostgresDsn, SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class SettingsSchema(BaseSettings):
+    model_config = SettingsConfigDict(env_file="app\.env",
+                                      env_file_encoding="utf-8")
+
+    DATABASE_URL: PostgresDsn
+    DATABASE_PASS: SecretStr
+    DATABASE_USR: str
+    CORS_ALLOWED_ORIGINS: list[str]
+    CORS_ALLOWED_METHODS: list[str]
