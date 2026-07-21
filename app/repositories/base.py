5e28c92b49153eed0835598e78f0aef7a872e9a4
n1ck-L <nikitalut@yandex.ru>
2026-07-21 16:42:14 +0300

    вынесен общий класс для репозиториев

---
+++ app/repositories/base.py
@@ -0,0 +1,21 @@
+from typing import Generic, TypeVar
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+ModelType = TypeVar("ModelType")
+
+class BaseRepository(Generic[ModelType]):
+    model: type[ModelType]
+
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_all(self) -> list[ModelType]:
+        return self.db.scalars(select(self.model)).all()
+    
+    def get_by_id(self, id: str) -> ModelType | None:
+        return self.db.get(self.model, id)
+    
+    def delete(self, inst: ModelType) -> None:
+        self.db.delete(inst)
--- app/repositories/user.py
+++ app/repositories/user.py
@@ -1,22 +1,10 @@
-from sqlalchemy import select
-from sqlalchemy.orm import Session
-
 from app.models.user import UserORM
+from app.repositories.base import BaseRepository
 
-class UserRepository:
-    def __init__(self, db: Session):
-        self.db = db
-        
-    def get_all(self) -> list[UserORM]:
-        return self.db.scalars(select(UserORM)).all()
-    
-    def get_by_id(self, user_id: str) -> UserORM:
-        return self.db.get(UserORM, user_id)
+class UserRepository(BaseRepository[UserORM]):
+    model = UserORM
     
     def create(self, email: str, login: str, password: str) -> UserORM:
         new = UserORM(email=email, login=login, password=password)
         self.db.add(new)
         return new
-    
-    def delete(self, inst: UserORM) -> None:
-        self.db.delete(inst)
