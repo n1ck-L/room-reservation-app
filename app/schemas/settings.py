8fc41c8a8b85122aaf516fde53932b8f28f5a985
n1ck-L <nikitalut@yandex.ru>
2026-07-25 21:08:40 +0300

    изменено расположение .env файла

--- app/schemas/settings.py
+++ app/schemas/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class SettingsSchema(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=r"app\.env", env_file_encoding="utf-8"
+        env_file=r".env", env_file_encoding="utf-8"
     )
 
     DATABASE_URL: PostgresDsn
